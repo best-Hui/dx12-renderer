724e4ddccd34779731a25b1cbdc47377b53c2d69
best-Hui <best-Hui@users.noreply.github.com>
2026-07-22 09:34:47 +0800

    Isolate SM6.10 LinAlg runtime path

--- CMakeIncludes/CopyDLLs.cmake
+++ CMakeIncludes/CopyDLLs.cmake
@@ -6,11 +6,21 @@ if (NOT TARGET CopyRuntimeDlls)
                 COMMAND ${CMAKE_COMMAND} -E copy_if_different "${CMAKE_SOURCE_DIR}/WinPixEventRuntime/bin/WinPixEventRuntime.dll" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/$<CONFIG>"
                 COMMAND ${CMAKE_COMMAND} -E copy_if_different "${CMAKE_SOURCE_DIR}/DXC/dxil.dll" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/$<CONFIG>"
                 COMMAND ${CMAKE_COMMAND} -E copy_if_different "${CMAKE_SOURCE_DIR}/DXC/dxcompiler.dll" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/$<CONFIG>"
+                VERBATIM
+                )
+endif()
+
+add_dependencies(${TARGET_NAME} CopyRuntimeDlls)
+
+if (DX12_RENDERER_ENABLE_SM610_LINALG AND NOT TARGET CopyD3D12AgilityRuntime)
+        add_custom_target(CopyD3D12AgilityRuntime
                 COMMAND ${CMAKE_COMMAND} -E make_directory "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/$<CONFIG>/D3D12"
                 COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_SOURCE_DIR}/D3D12" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/$<CONFIG>/D3D12"
                 VERBATIM
                 )
 endif()
 
-add_dependencies(${TARGET_NAME} CopyRuntimeDlls)
+if (TARGET CopyD3D12AgilityRuntime)
+        add_dependencies(${TARGET_NAME} CopyD3D12AgilityRuntime)
+endif()
 # Modify End
